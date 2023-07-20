da7e907370e2cda40c0e6427dc8e73e58cffd320
P <tatakornaow@gmail.com>
2023-07-20 16:39:26 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,22 @@ import { useState } from "react";
 export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
+  const [count, setCount] = useState({ all: 0, done: 0 });
+  // const handleSetCount = (key,value) =>{
+  //   setCount(prev=>({...prev,[key]}))
+  // }
 
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
+    setCount((prev) => ({ ...prev, all: prev.all + 1 }));
   };
 
   const deleteTask = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    setCount((prev) => ({ ...prev, all: prev.all - 1 }));
   };
 
   const toggleDoneTask = (taskId) => {
@@ -41,7 +47,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({count.all}) Done ({tasks.filter((x) => x.completed).length})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +66,11 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer
+        year="2023"
+        fullName="Tayakorn Aowrattanakul"
+        studentId="650610763"
+      />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,6 +9,12 @@ export const Task = ({
     deleteTaskFunc(id);
   };
 
+  const doneBtnOnClick = () => {
+    toggleDoneTaskFunc(id);
+  };
+
+  const line = completed ? "text-decoration-line-through" : "";
+
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
       {/*
@@ -17,8 +23,10 @@ export const Task = ({
         But if task is not completed : 
         <span>{title}</span>
       */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      <span className={line}>{title}</span>
+      <button className="btn btn-success" onClick={doneBtnOnClick}>
+        Done
+      </button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
